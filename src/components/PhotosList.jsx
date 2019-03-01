e33b0cf74898b721a4e9c0764b87e2484f16e341
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 10:00:02 +0000

    recived required data from prams in photolist.jsx

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
 class PhotosList extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    const {
+      match: { params }
+    } = this.props;
+    this.state = {
+      AlbumID: params.id
+    };
+  }
+
+  componentDidMount = () => {
+    console.log(this.state.AlbumID);
+  };
   render() {
     return <h1>PhotosList</h1>;
   }
