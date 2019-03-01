576d8d5452db2aebd08b56eb29a3510a129acf49
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 10:13:34 +0000

    rendered photo elements

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -8,17 +8,41 @@ class PhotosList extends Component {
       match: { params }
     } = this.props;
     this.state = {
-      AlbumID: params.id
+      AlbumID: params.id,
+      photoData: []
     };
   }
 
   componentDidMount = () => {
-    api.getDataList(`?albumId=${this.state.AlbumID}`).then(result => {
-      console.log(result.data);
+    api.getDataList(`?albumId=${this.state.AlbumID}`).then(list => {
+      console.log(1);
+      this.setState({ photoData: list.data });
     });
   };
+
+  imageGridGenerator = x => {
+    return (
+      <div key={x.id} className="col-lg-2 col-md-4 col-sm-4 col-xs-6 grid-mar">
+        <img
+          src={x.thumbnailUrl}
+          onClick={this.imageSelector}
+          id={x.id}
+          alt={x.title}
+        />
+        <span>{x.title}</span>
+      </div>
+    );
+  };
+
   render() {
-    return <h1>PhotosList</h1>;
+    return (
+      <div className="container">
+        <h1>PhotosList</h1>
+        <div className="row">
+          {this.state.photoData.map(x => this.imageGridGenerator(x))}
+        </div>
+      </div>
+    );
   }
 }
 
