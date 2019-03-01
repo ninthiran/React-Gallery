ddcb970a222ab3216718965f3c5536fcef997c3d
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 09:57:57 +0000

    implemented Routeing in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AlbumList from "./components/AlbumList";
 import PhotosList from "./components/PhotosList";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import Route from "react-router-dom/Route";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -12,8 +13,11 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <AlbumList />
-          <PhotosList />
+          <Route path="/" exact component={AlbumList} />
+          <Route
+            path="/album/:id"
+            render={props => <PhotosList {...props} />}
+          />
         </div>
       </Router>
     );
