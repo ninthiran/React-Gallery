34d6240ab579bf73a68f9f8d75768a8d2c12ae28
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 10:25:00 +0000

    modified Gridbox to make it more generic to accept both components

--- src/components/GribBox.jsx
+++ src/components/GribBox.jsx
@@ -6,10 +6,22 @@ class GridBox extends Component {
   render() {
     return (
       <div className="col-lg-2 col-md-4 col-sm-4 col-xs-6 grid-mar">
-        <NavLink to={`/album/${this.props.albumId}`}>
-          <img src={this.props.thumbnailUrl} alt={this.props.title} />
-          <span>Album - {this.props.albumId}</span>
-        </NavLink>
+        {this.props.linkEnable == true ? (
+          <NavLink to={`/album/${this.props.albumId}`}>
+            <img src={this.props.thumbnailUrl} alt={this.props.title} />
+            <span>Album - {this.props.albumId}</span>
+          </NavLink>
+        ) : (
+          <div>
+            <img
+              src={this.props.thumbnailUrl}
+              alt={this.props.title}
+              onClick={this.imageSelector}
+              title={this.props.url}
+            />
+            <p>{this.props.label}</p>
+          </div>
+        )}
       </div>
     );
   }
