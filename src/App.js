d2b8a82b1cf9533f10b4c0ee40486813bc1782b9
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 09:27:48 +0000

    adding required packages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
-
 import AlbumList from "./components/AlbumList";
 import PhotosList from "./components/PhotosList";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AlbumList />
-        <PhotosList />
-      </div>
+      <Router>
+        <div className="App">
+          <AlbumList />
+          <PhotosList />
+        </div>
+      </Router>
     );
   }
 }
