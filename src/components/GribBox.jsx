aaec04fc28e7ad538f1b8316f3bd385280d0bed5
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 10:16:54 +0000

    created new 'GridBox' which used to generate grid boxes to reuseable

---
+++ src/components/GribBox.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class GridBox extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Grid Box Layout</h1>
+      </div>
+    );
+  }
+}
+
+export default GridBox;
